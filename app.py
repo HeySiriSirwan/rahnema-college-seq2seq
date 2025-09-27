47731b12d49bb025c36d3010301b990df8e1b95c
Sirwan <sirwanamini04@gmail.com>
2025-09-27 21:20:09 +0330

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello1():
     return 'test'
+
+@app.route('/endpoint')
+def hello2():
+    return 'hellp'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
